48ad79a5bd44a432053909591790b6ef2d4d4f5f
91808 <shreyashhake4@gmail.com>
2022-06-03 21:06:19 +0530

    first commit

---
+++ JS-Fundamentals 1/Starter/script.js
@@ -0,0 +1,57 @@
+// let js = "amazing";
+// if (js === "amazing") alert("Here we go !");
+
+console.log("Using console to print the values : ")
+console.log(50 + 32 + 42 * 2);
+
+
+let age = 20;
+console.log(age);
+
+// prefer camelCase for naming convention in JS
+console.log(" ")
+console.log("Diving into the variables : ")
+let firstName = "Shreyash";
+console.log(firstName);
+
+let secondName = "Shreyash";
+console.log(secondName);
+
+let currentState = "Student";
+let finalState = "Developer";
+
+console.log(currentState);
+console.log(finalState);
+
+// Assingment on variables 
+console.log(" ")
+console.log("Assingment on variables : ")
+let country = "India";
+let continent = "Asian";
+let population = "1340 Million"
+
+console.log(country + " is in " + continent + " and has the population of " + population)
+
+// Data types in JavaScript
+console.log(" ")
+console.log("Checking the datatype : ")
+let price = 200
+let item = "watch"
+let available = true
+let seller
+let buyer = null
+
+console.log(typeof price)
+console.log(typeof item)
+console.log(typeof available)
+console.log(typeof seller)
+console.log(typeof buyer)
+console.log("yes this is a bug ... that typeof 'null' is Object")
+
+console.log(" ")
+console.log("Assingment Data type")
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland)
+console.log(typeof language)
