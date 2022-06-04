c1effcbb222fdafc2c3d1889826be68dbe5b353f
91808 <shreyashhake4@gmail.com>
2022-06-04 19:16:40 +0530

    Added new file

--- JS-Fundamentals 1/Starter/script.js
+++ JS-Fundamentals 1/Starter/script.js
@@ -24,8 +24,8 @@ console.log(currentState);
 console.log(finalState);
 
 // Assingment on variables 
-console.log(" ")
-console.log("Assingment on variables : ")
+console.log(" ");
+console.log("Assingment on variables : ");
 let country = "India";
 let continent = "Asian";
 let population = "1340 Million"
@@ -33,25 +33,41 @@ let population = "1340 Million"
 console.log(country + " is in " + continent + " and has the population of " + population)
 
 // Data types in JavaScript
-console.log(" ")
-console.log("Checking the datatype : ")
+console.log(" ");
+console.log("Checking the datatype : ");
 let price = 200
 let item = "watch"
 let available = true
 let seller
 let buyer = null
 
-console.log(typeof price)
-console.log(typeof item)
-console.log(typeof available)
-console.log(typeof seller)
-console.log(typeof buyer)
-console.log("yes this is a bug ... that typeof 'null' is Object")
+console.log(typeof price);
+console.log(typeof item);
+console.log(typeof available);
+console.log(typeof seller);
+console.log(typeof buyer);
+console.log("yes this is a bug ... that typeof 'null' is Object");
 
-console.log(" ")
-console.log("Assingment Data type")
+console.log(" ");
+console.log("Assingment Data type");
 let isIsland = true;
 let language;
 
-console.log(typeof isIsland)
-console.log(typeof language)
+console.log(typeof isIsland);
+console.log(typeof language);
+
+// let , const and var
+console.log(" ");;
+console.log("variables assingend with 'let' can be resassinged");
+console.log("variables assinged with 'var' can also be reassinged but it is a old way");
+console.log("variables assinged with 'const' can not be reassinged");
+
+// Assingment on let, const and var
+const primaryLanguage = "Marathi";
+let secondaryLanguage = "Hindi";
+console.log(" ");
+console.log(primaryLanguage);
+console.log(secondaryLanguage);
+
+secondaryLanguage = "English";
+console.log(secondaryLanguage);
