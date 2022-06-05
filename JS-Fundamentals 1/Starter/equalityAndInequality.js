12be0e51c7c8c5cec0386580bc84d125aac9105c
91808 <shreyashhake4@gmail.com>
2022-06-05 23:06:34 +0530

    new commit ✨

---
+++ JS-Fundamentals 1/Starter/equalityAndInequality.js
@@ -0,0 +1,20 @@
+console.log(`
+>> Equality and inequality : `)
+
+let sellingPrice = Number(prompt("How much money do you have ??"));
+
+if (sellingPrice === 125) {
+    console.log("You can purchase it !!")
+} else if (sellingPrice > 125) {
+    console.log("Wow, you can buy few more things ")
+} else {
+    console.log("Ohhh.. you need more money")
+}
+
+// calculation age 
+age = Number(prompt("What is your age !"));
+
+if (age !== 18) console.log("You are not 18!!")
+else {
+    console.log("You are 18!!");
+}
