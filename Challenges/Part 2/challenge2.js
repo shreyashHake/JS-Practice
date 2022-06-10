100aace0c017b9fa897981729138d07c57c3a158
91808 <shreyashhake4@gmail.com>
2022-06-10 22:53:36 +0530

    Changes

--- Challenges/Part 2/challenge2.js
+++ Challenges/Part 2/challenge2.js
@@ -2,13 +2,14 @@ console.log(`
 >> Challenge 2 : `)
 
 function calcTip1(bill) {
-    let tip;
-    if (50 <= bill && bill >= 300) {
-        tip = 1500 / bill;
-    } else {
-        tip = 2000 / bill;
-    }
-    return tip;
+    return Number((50 <= bill && bill >= 300) ? 0.15 * bill : 0.2 * bill);
 }
 
 console.log(calcTip1(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip1(bills[0]), calcTip1(bills[1]), calcTip1(bills[2])];
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills , tips, total);
