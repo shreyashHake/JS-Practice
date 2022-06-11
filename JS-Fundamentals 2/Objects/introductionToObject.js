b363272ea1a39804edd3cb340dc281256d23b208
91808 <shreyashhake4@gmail.com>
2022-06-11 22:13:48 +0530

    new commit

---
+++ JS-Fundamentals 2/Objects/introductionToObject.js
@@ -0,0 +1,15 @@
+console.log(`
+>> Objects in javaScript : `)
+
+// objects are like arrays in java but object have value for each element
+
+const buddy = ['Rushabh', 'Tanmay', 'Mehul', 'Sharukh'];
+
+const shreyash = [
+    firstName = 'Shreyash',
+    lastName = 'Hake',
+    age = 20,
+    height = '200cm'
+]
+
+console.log(shreyash);
