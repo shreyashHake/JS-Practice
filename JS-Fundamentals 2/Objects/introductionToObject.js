4119131cf1527c4d8e7d2dca42402f070caa0624
91808 <shreyashhake4@gmail.com>
2022-06-12 22:10:37 +0530

    modified

--- JS-Fundamentals 2/Objects/introductionToObject.js
+++ JS-Fundamentals 2/Objects/introductionToObject.js
@@ -2,14 +2,33 @@ console.log(`
 >> Objects in javaScript : `)
 
 // objects are like arrays in java but object have value for each element
+// order is not maintained in object ....
+const buddy = ['Rushabh', 'Tanmay', 'Mehul', 'Sharukh', 'Harshal'];
 
-const buddy = ['Rushabh', 'Tanmay', 'Mehul', 'Sharukh'];
+// Eg 1 : 
 
-const shreyash = [
-    firstName = 'Shreyash',
-    lastName = 'Hake',
-    age = 20,
-    height = '200cm'
-]
+const movies = {
+    bollywood: ['Don', 'PK', 'Dhoom'],
+    hollywood: ['DonJon', 'SK', 'Zoom'],
+    tollywood: ['KgF', 'RRR']
+};
 
-console.log(shreyash);
+console.log(movies.bollywood)
+console.log(movies.hollywood)
+console.log(movies.tollywood)
+
+// Eg 2 : 
+
+const shreyashObject = {
+    firstName: "Shreyash",
+    lastName: "Hake",
+    age: 2022 - 2002,
+    friends: ['Rushabh', 'Tanmay', 'Mehul', 'Sharukh', 'Harshal'],
+    college: 'GHRCE'
+};
+
+console.log(shreyashObject);
+console.log(shreyashObject.age);
+console.log(shreyashObject.firstName);
+console.log(shreyashObject.lastName);
+console.log(`I am ${shreyashObject.firstName} ${shreyashObject.lastName}, from ${shreyashObject.college}.`)
