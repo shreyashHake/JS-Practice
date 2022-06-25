1d979ae43d52f10c5936d04d7b66a42859b22ebd
91808 <shreyashhake4@gmail.com>
2022-06-25 21:59:23 +0530

    Added new project

--- Project 3/script.js
+++ Project 3/script.js
@@ -4,4 +4,5 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 
-score0El === 0;
+score0El.textContent === 0;
+score0El.textContent === 0;
